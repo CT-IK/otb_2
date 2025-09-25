ac7c2214f7e0b0404b31adda49df9eaf0eb20c6c
Anatoly <anatolii.kozmin@inbox.ru>
2025-09-26 02:04:55 +0300

    refactoring 6

--- migration/versions/add_unique_slot_limits_faculty_date_time.py
+++ migration/versions/add_unique_slot_limits_faculty_date_time.py
@@ -1,7 +1,26 @@
+"""add unique constraint to slot_limits
+
+Revision ID: add_unique_slot_limits_faculty_date_time
+Revises: add_slot_limits_2025
+Create Date: 2025-09-26
 """
-Alembic migration: добавление уникального ограничения на slot_limits (faculty_id, date, time_slot)
-"""
+revision = 'add_unique_slot_limits_faculty_date_time'
+down_revision = 'add_slot_limits_2025'
+branch_labels = None
+depends_on = None
+
 from alembic import op
 
-"""
+def upgrade():
     op.create_unique_constraint(
+        "uq_slot_limits_faculty_date_time",
+        "slot_limits",
+        ["faculty_id", "date", "time_slot"]
+    )
+
+def downgrade():
+    op.drop_constraint(
+        "uq_slot_limits_faculty_date_time",
+        "slot_limits",
+        type_="unique"
+    )
